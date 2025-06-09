70668c8b376a07f918c5e24277261b45411b1ed3
Moussa <salisoumoussa1@gmail.com>
2025-06-09 16:19:42 +0200

    Added Last 5 Cars wiht Brands CQRS

---
+++ Core/RentCar.Application/Features/CQRS/Handlers/CarHandlers/GetLast5CarWithBrandQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using RentCar.Application.Features.CQRS.Results.CarResults;
+using RentCar.Application.Interfaces.CarInterfaces;
+
+namespace RentCar.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class GetLast5CarWithBrandQueryHandler
+    {
+        private readonly ICarRepository _carRepository;
+
+        public GetLast5CarWithBrandQueryHandler(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public List<GetLast5CarsWithBrandQueryResult> Handler()
+        {
+            var values = _carRepository.GetLast5CarsWithBrands();
+            return values.Select(c => new GetLast5CarsWithBrandQueryResult
+            {
+                CarId = c.CarId,
+                BrandId = c.BrandId,
+                BrandName = c.Brand.Name,
+                Model = c.Model,
+                CoverImageUrl = c.CoverImageUrl,
+                BigImageUrl = c.BigImageUrl,
+                Fuel = c.Fuel,
+                Km = c.Km,
+                Luggage = c.Luggage,
+                Seat = c.Seat,
+                Transmission = c.Transmission
+            }).ToList();
+        }
+    }
+}
---
+++ Core/RentCar.Application/Features/CQRS/Results/CarResults/GetLast5CarsWithBrandQueryResult.cs
@@ -0,0 +1,17 @@
+﻿namespace RentCar.Application.Features.CQRS.Results.CarResults
+{
+    public class GetLast5CarsWithBrandQueryResult
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string CoverImageUrl { get; set; } = string.Empty;
+        public int Km { get; set; }
+        public string Transmission { get; set; } = string.Empty;
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; } = string.Empty;
+        public string BigImageUrl { get; set; } = string.Empty;
+    }
+}
